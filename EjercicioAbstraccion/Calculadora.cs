7d4a4f7f0e23954297c2aa7c56d9639640e6c62b
MGomez97 <manuelgomez19010@gmail.com>
2024-08-02 20:15:13 -0600

    Update .gitignore

---
+++ EjercicioAbstraccion/Calculadora.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+class CalculadoraMatematica : Matematicas
+{
+    private double numero1;
+    private double numero2;
+
+    // Implementación de los métodos abstractos
+    public override double Operacion1(double num1, double num2)
+    {
+        return num1 + num2;
+    }
+
+    public override double Operacion2(double num1, double num2)
+    {
+        return num1 - num2;
+    }
+
+    public override double Operacion3(double num1, double num2)
+    {
+        return num1 * num2;
+    }
+
+    public override double Operacion4(double num1, double num2)
+    {
+        if (num2 != 0)
+            return num1 / num2;
+        else
+            throw new ArgumentException("No se puede dividir por cero.");
+    }
+
+    // Método para asignar números ingresados por el usuario
+    public void AsignarNumeros(double num1, double num2)
+    {
+        numero1 = num1;
+        numero2 = num2;
+    }
+
+    // Método para realizar la operación seleccionada
+    public void RealizarOperacion(int opcion)
+    {
+        double resultado = 0;
+
+        switch (opcion)
+        {
+            case 1:
+                resultado = Operacion1(numero1, numero2);
+                break;
+            case 2:
+                resultado = Operacion2(numero1, numero2);
+                break;
+            case 3:
+                resultado = Operacion3(numero1, numero2);
+                break;
+            case 4:
+                resultado = Operacion4(numero1, numero2);
+                break;
+            default:
+                Console.WriteLine("Opción no válida.");
+                return;
+        }
+
+        Imprimir(resultado);
+    }
+}
--- EjercicioAbstraccion/Program.cs
+++ EjercicioAbstraccion/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CalculadoraMatematica calculadora = new CalculadoraMatematica();
+
+        Console.WriteLine("Ingrese el primer número:");
+        double num1 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el segundo número:");
+        double num2 = double.Parse(Console.ReadLine());
+
+        calculadora.AsignarNumeros(num1, num2);
+
+        Console.WriteLine("Seleccione la operación a realizar:");
+        Console.WriteLine("1. Suma");
+        Console.WriteLine("2. Resta");
+        Console.WriteLine("3. Multiplicación");
+        Console.WriteLine("4. División");
+
+        int opcion = int.Parse(Console.ReadLine());
+
+        calculadora.RealizarOperacion(opcion);
+
+        Console.ReadLine(); // Para que la consola no se cierre inmediatamente
+    }
+}
