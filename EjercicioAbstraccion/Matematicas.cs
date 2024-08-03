279a9b49d171091ba7ac0a71ddcaa8660ea9a7e6
MGomez97 <manuelgomez19010@gmail.com>
2024-08-02 20:12:31 -0600

    Create Matematicas.cs

---
+++ EjercicioAbstraccion/Matematicas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+abstract class Matematicas
+{
+    // Métodos abstractos
+    public abstract double Operacion1(double num1, double num2);
+    public abstract double Operacion2(double num1, double num2);
+    public abstract double Operacion3(double num1, double num2);
+    public abstract double Operacion4(double num1, double num2);
+
+    // Método concreto para imprimir
+    public void Imprimir(double resultado)
+    {
+        Console.WriteLine("El resultado es: " + resultado);
+    }
+}
